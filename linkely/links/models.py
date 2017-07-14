043b8c94baf21382b7f91e7820022119a7a10060
Narjis Hachilif <hnarjis@gmail.com>
2017-07-14 22:16:48 +0200

    Create article model

--- linkely/links/models.py
+++ linkely/links/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=150)
+    url = models.URLField(max_length=500)
+    description = models.TextField()
+    tags = models.CharField(max_length=50)
+    date = models.DateTimeField('date added')
+
+# TODO: Add model for the user
