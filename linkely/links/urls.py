550204f493ee38b5b84be40239690daee2575e53
André Laszlo <andre@laszlo.nu>
2017-07-15 23:29:44 +0200

    Add search endpoint

--- linkely/links/urls.py
+++ linkely/links/urls.py
@@ -6,4 +6,5 @@ app_name = 'links'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^add$', views.add, name='add'),
+    url(r'^search$', views.search, name='search'),
 ]
--- linkely/links/views.py
+++ linkely/links/views.py
@@ -1,6 +1,8 @@
 from django.views import generic
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import render
+from elasticsearch import Elasticsearch
 from .models import Article
 from .scraper import scrape
 
@@ -10,6 +12,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Article.objects.order_by('-date')[:20]
+
 
 def add(request):
     try:
@@ -21,3 +24,35 @@ def add(request):
         article.save()
         scrape(article)
         return HttpResponseRedirect(reverse('links:index'))
+
+
+def search(request):
+    template_name = 'links/search_result.html'
+    context_object_name = 'search_results'
+
+    context = {'search_results': None, 'error': None}
+
+    querystring = request.GET.get('q', '')
+    # TODO: empty query should show zero results
+    # TODO: make an elasticsearch client factory or something
+    try:
+        es = Elasticsearch(['http://elastic:changeme@localhost:9200'])
+        query = {
+            "query": {
+                "multi_match" : {
+                    "query": querystring,
+                    "fields": ["title", "body"]
+                }
+            }
+        }
+        res = es.search(index="articles", body=query)
+    except Exception as ex:
+        context['error': 'Something went wrong :( %r' % ex]
+
+    articles = [Article.objects.get(pk=article['_id'])
+                for article
+                in res.get('hits', []).get('hits', [])]
+
+    context['search_results'] = articles
+
+    return render(request, 'links/search_result.html', context)
