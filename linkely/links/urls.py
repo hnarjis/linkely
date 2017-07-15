2bc3a9ff1ab6406beed7cdb5396226a62f82fb86
Narjis Hachilif <hnarjis@gmail.com>
2017-07-15 16:41:29 +0200

    Add URL form

--- linkely/links/urls.py
+++ linkely/links/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'links'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^index$', views.index, name='index'),
+    url(r'^add$', views.add, name='add'),
 ]
--- linkely/links/views.py
+++ linkely/links/views.py
@@ -4,3 +4,15 @@ from .models import Article
 def index(request):
     latest_articles = Article.objects.order_by('-date')[:20]
     return render(request, 'links/index.html', {'latest_articles': latest_articles})
+
+def add(request):
+    try:
+        url = request.POST['url']
+    except (KeyError):
+        raise Exception("Missing URL.")
+    else:
+        pass
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        # return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
