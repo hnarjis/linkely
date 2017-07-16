6e274a2b3d33a8ea22b1f3531ad3c1c293a4be60
Narjis Hachilif <hnarjis@gmail.com>
2017-07-16 23:37:41 +0200

    Style list of article links

---
+++ linkely/links/templatetags/links_extras.py
@@ -0,0 +1,9 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+@register.filter(name='domain_name')
+@stringfilter
+def domain_name(value):
+    return value.split('//')[-1].split('/')[0].replace('www.','')
