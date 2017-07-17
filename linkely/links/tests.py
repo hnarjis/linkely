84b93b98cf5290e618738fb84a3307c1c6c77059
Narjis Hachilif <hnarjis@gmail.com>
2017-07-18 01:11:44 +0200

    Add some tests

--- linkely/links/tests.py
+++ linkely/links/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Article
+from .scraper import guess_title
 
-# Create your tests here.
+class ArticleTestCase(TestCase):
+    def test_to_string(self):
+        article = Article.objects.create(title='example', url='http://www.example.com')
+        self.assertEqual(str(article), 'example - http://www.example.com')
+
+class ScraperTestCase(TestCase):
+    def test_title_can_be_guessed(self):
+        title = guess_title(None, 'http://www.example.com/hello/this_is_an_article-example')
+        self.assertEqual(title, 'This is an article example')
