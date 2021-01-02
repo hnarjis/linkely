f8336db7cfdba29c7fefbff2449943a06266bcf6
André Laszlo <andre@laszlo.nu>
2021-01-02 21:16:08 +0100

    Improve error handling and don't allow resubmitting links

--- linkely/links/scraper.py
+++ linkely/links/scraper.py
@@ -4,38 +4,41 @@ import re
 from .wiring import es_client
 
 
+class ScraperError(Exception):
+    pass
+
+
 def guess_title(document, url):
     try:
         return document.find("title").text
     except:
         pass
 
-    document_name = url.split('/')[-1]
-    document_name = re.sub('[-_]', ' ', document_name)
+    document_name = url.split("/")[-1]
+    document_name = re.sub("[-_]", " ", document_name)
     return document_name.capitalize()
 
 
 def scrape(article):
-    response = requests.get(article.url)
-    document = None
-    if response.ok:
-        document = BeautifulSoup(response.content, 'lxml')
-    title = guess_title(document, article.url)
-    if title:
-        article.title = title
-    index(article, document)
+    try:
+        response = requests.get(article.url)
+        document = None
+        if response.ok:
+            document = BeautifulSoup(response.content, "lxml")
+        title = guess_title(document, article.url)
+        if title:
+            article.title = title
+        index(article, document)
+    except Exception as err:
+        raise ScraperError() from err
 
 
 def index(article, document):
     es = es_client()
 
-    doc = {
-        'title': article.title,
-        'url': article.url,
-        'body': document.get_text()
-    }
+    doc = {"title": article.title, "url": article.url, "body": document.get_text()}
 
-    res = es.index(index="articles", doc_type='article', id=article.id, body=doc)
+    res = es.index(index="articles", doc_type="article", id=article.id, body=doc)
 
     # NOTE: remove this for high volume sites
     es.indices.refresh(index="articles")
