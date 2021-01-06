8b2d04d4361f05e1036dac935e79782f248a60b6
Narjis Hachilif <hnarjis@gmail.com>
2021-01-06 19:06:52 +0100

    Auto-formatting

--- linkely/links/urls.py
+++ linkely/links/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'links'
+app_name = "links"
 urlpatterns = [
-    url(r'^add$', views.add, name='add'),
-    url(r'^search$', views.search, name='search'),
-    url(r'^user/(?P<username>\w+)$', views.user, name='user'),
+    url(r"^add$", views.add, name="add"),
+    url(r"^search$", views.search, name="search"),
+    url(r"^user/(?P<username>\w+)$", views.user, name="user"),
 ]
