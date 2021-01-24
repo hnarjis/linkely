c30c039c57d130317150820a481117fb1b53109c
André Laszlo <andre@laszlo.nu>
2021-01-24 21:00:44 +0100

    Add endpoint for registering new users

--- linkely/linkely/settings.py
+++ linkely/linkely/settings.py
@@ -172,7 +172,6 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 50,
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
--- linkely/links/serializers.py
+++ linkely/links/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from links.models import Article
 from links.validators import password_validator
 
@@ -22,12 +23,12 @@ class ArticleSerializer(serializers.ModelSerializer):
 class UserRegistrationSerializer(serializers.Serializer):
     first_name = serializers.CharField(max_length=50)
     last_name = serializers.CharField(max_length=50)
-    username = serializers.CharField(max_length=20)
+    username = serializers.CharField(
+        max_length=20, validators=[UniqueValidator(queryset=User.objects.all())]
+    )
     email = serializers.EmailField()
     password = serializers.CharField(validators=[password_validator])
 
     def create(self, validated_data):
+        print(validated_data)
         return User.objects.create_user(**validated_data, is_active=False)
-
-    def validate_password(self, value):
-        pass
