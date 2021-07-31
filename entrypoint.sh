31f0c7674c1502108c28a1791f9f5c2a08c010f9
André Laszlo <andre@laszlo.nu>
2021-07-31 23:05:34 +0200

    Use gunicorn and serve static files separately

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ cd /src/linkely &&
     poetry install --no-dev &&
     poetry run ./manage.py migrate &&
     poetry run ./manage.py collectstatic --no-input &&
-    poetry run ./manage.py runserver 0.0.0.0:8000
+    poetry run gunicorn --bind 0.0.0.0:8000 linkely.wsgi
--- linkely/linkely/settings.py
+++ linkely/linkely/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "http://localhost:8001/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
